747f5f879b3043837c77ad1f7d68a9527c89034a
root <root@scorpion.scorpion>
2020-12-18 12:41:57 -0300

    versão 1.0

---
+++ scorpionpack.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+echo CONHECIMENTO NÃO É CRIME 
+sleep 2
+apt install figlet 
+apt install toilet
+apt install sl
+sl 
+toilet -f mono12 -F metal scorpion 
+sleep 3
+echo $red"XXXX'            .:ok0XNMMMMMMMWNKko:.             'KXXX"
+echo $red"XXXX'         .c0MMMMMMMMMMMMMMMMMMMMMKo.          'KXXX"
+echo $red"XXXX'        :XMMMMMMMMMMMMMMMMMMMMMMMMMWO;        'KXXX"
+echo $red"XXXX'     .cKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0,      'KXXX"
+echo $red"XXXX'    lNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWd.    'KXXX"
+echo $red"XXXX'   xMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0.   'KXXX"
+echo $red"XXXX'  ;MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0.  'KXXX"
+echo $red"XXXX'  xMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMl  'KXXX"
+echo $red"XXXX'  OMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMx  'KXXX"
+echo $red"XXXX   OMMOlxKMMMMMMMMMMMMMMMMMMMMMMMMMMMWKkdkMMo   KXXX"
+echo $red"XXXX'  oMM:   .ckWMMMMMMMMMMMMMMMMMMMMXd,.   ;MM;  'KXXX"
+echo $red"XXXX'  ;MMx      .:kNMMMMMMMMMMMMMMNx,       xMX.  'KXXX"
+echo $red"XXXX'   XMM,        .,lkXMMMMMMN0o,.        ;WMc   'KXXX"
+echo $red"XXXX'     MWx;..        .:XWMK:.        ..;xWMX.   'KXXX"
+echo $red"XXXX'    .0MMMMWXK0kxxOKWMxdokMWKkxxk0KXWMMMMMc    'KXXX"
+echo $red"XXXX'    .oNMMMMMMMMMMMMMx l;.kMMMMMMMMMMMMMMW.    'KXXX"
+echo $red"XXXX'    ,MMMMMMMMWNWMMMK ,NX' KMMMMMMWXNMMMMMx    'KXXX"
+echo $red"XXXX'    oMMMMMNd,. :MMMMkKMMkxMMMMMO'   :MMMN,    'KXXX"
+echo $red"XXXX'    .oWKxOo'   :MMMMMMMMMMMMMMMk    :XO:.     'KXXX"
+echo $red"XXXX'               cMMN MMX MW oNMM;              'KXXX"
+echo $red"XXXK'                0MM MM0 MM NMMo               'XXXX"
+echo $red"XXXX'                xMM MMK MM WMM.               'KXXX"
+echo $red"XXXX'                dMM MMK MM MMW.               'KXXX"
+echo $red"XXXX'                oMM MMK MM MMN.               'KXXX"
+echo $red"XXXX'                lMM NM0 MW MMX                'KXXX"
+echo $red"XXXX'                :MM XMO MX MM0                'KXXX"
+echo $red"XXXX'                 MM 0Mx M0 MMx                'KXXX"
+echo $red"XXXX'                 lO OMc Mo WX;                'KXXX"
+echo $red"XXXX'                                              'KXXX"
+echo $red"XXXX'                𝕤𝕔𝕠𝕣𝕡𝕚𝕠𝕟 𝕜𝕚𝕟𝕘                 'KXXX"
+echo $red"XXXX:..............................................:KXXX"
+echo $red"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+echo $red"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+echo $red"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+sleep 2
+figlet ferramentas basicas
+apt install git
+apt install python
+apt install python2
+apt install python3
+apt install python3-pip
+apt install pip3
+apt install curl
+apt install w3m
+apt install bash
+apt install bash
+apt install wget
+apt install tor
+apt install perl
+apt install cowsay
+apt install screenfetch
+apt install cmatrix
+figlet ferramentas de médio uso
+echo CONHECIMENTO  NÃO É CRIME
+sleep 2 
+figlet red_hawk
+git clone https://github.com/Tuhinshubhra/RED_HAWK.git
+figlet credmap
+git clone https://github.com/lightos/credmap.git
+figlet recondog 
+git clone https://github.com/UltimateHackers/ReconDog.git
+figlet ngrok 
+git clone https://github.com/inconshreveable/ngrok.git
+figlet consulta-v3
+git clone https://github.com/T3rMuX0/consulta-v3.git
+figlet evil flamework
+git clone https://github.com/LOoLzeC/Evil-create-framework.git
+figlet ipmux
+git clone https://github.com/Amriez/ipmux
+figlet owscan
+git clone https://github.com/Gameye98/OWScan.git
+figlet torshammer
+git clone https://github.com/Karlheinzniebuhr/torshammer.git
+figlet vbug
+git clone https://github.com/Gameye98/vbug.git
+figlet tstyling
+git clone https://github.com/darkwarrior3/tstyling
+figlet phoneinfoga
+git clone https://github.com/la-deep-web/Phoneinfoga.git
+figlet nikto
+git clone https://github.com/sullo/nikto
+figlet sqlmap
+git clone https://github.com/sqlmapproject/sqlmap
+figlet hammer
+git clone https://github.com/liorvh/hammer-1
+figlet nmap
+apt install nmap
+echo CONHECIMENTO NÃO É CRIME
+sleep 3
+cmatrix
